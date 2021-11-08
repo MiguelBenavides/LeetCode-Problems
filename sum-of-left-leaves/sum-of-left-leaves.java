53560ce9b08d5285647121c046389937236a387c
Miguel Benavides Banda <36273644+MiguelBenavides@users.noreply.github.com>
2021-11-08 13:45:09 -0600

    Time: 0 ms (100.00%), Space: 37 MB (24.69%) - LeetHub

--- sum-of-left-leaves/sum-of-left-leaves.java
+++ sum-of-left-leaves/sum-of-left-leaves.java
@@ -15,12 +15,13 @@
  */
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        return dfs(root, false);
+        return dfs(root, 0, false);
     }
     
-    public int dfs(TreeNode node, boolean isLeft){
+    public int dfs(TreeNode node, int sum, boolean isLeft){
         if(node == null) return 0;
-        if(node.left == null && node.right == null && isLeft) return node.val;
-        return dfs(node.left, true) + dfs(node.right, false);
+        if(node.left == null && node.right == null && isLeft) sum += node.val;
+        sum += dfs(node.left, sum, true) + dfs(node.right, sum, false);
+        return sum;
     }
 }
