0906a4e78f408540096f38a24ddda4ea2ade07d2
Miguel Benavides Banda <36273644+MiguelBenavides@users.noreply.github.com>
2021-11-30 15:39:05 -0600

    Time: 1 ms (95.00%), Space: 39.1 MB (91.16%) - LeetHub

--- permutations/permutations.java
+++ permutations/permutations.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(nums == null || nums.length == 0)
-            return result;
-        boolean[] used = new boolean[nums.length];
-        List<Integer> permutation = new ArrayList<>();
-        helper(nums, permutation, used, result);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int listSize;
+        result.add(new ArrayList<Integer>());
+        
+        for (int num : nums) {
+            listSize = result.size();
+            while (listSize > 0) {
+                List<Integer> permutation = result.remove(0);
+                for (int i = 0; i <= permutation.size(); i++) {
+                    List<Integer> newPerm = new ArrayList<Integer>(permutation);
+                    newPerm.add(i, num);
+                    result.add(newPerm);
+                }
+                listSize--;
+            }
+        }
+        
         return result;
-    }
-    
-    private void helper(int[] nums, List<Integer> permutation, boolean[] used, List<List<Integer>> result) {
-        if (permutation.size() == nums.length) {
-            result.add(new ArrayList<>(permutation));
-            return;
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i])
-                continue;
-            used[i] = true;
-            permutation.add(nums[i]);
-            helper(nums, permutation, used, result);
-            permutation.remove(permutation.size() - 1);
-            used[i] = false;
-        }
     }
 }
