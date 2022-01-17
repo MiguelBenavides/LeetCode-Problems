fa5ac5d9083cc409728d54254d6abcb5e9bf1e8a
Miguel Benavides Banda <36273644+MiguelBenavides@users.noreply.github.com>
2022-01-17 14:51:48 -0600

    Time: 0 ms (100.00%), Space: 48.2 MB (29.91%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,14 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        //Calculate the summation formula of n
-        int n = nums.length;
-        int sum = (n * (n + 1)) / 2;
+        int result = 0;
         
-        //The missing number will always be the summation minus all elements of nums
-        for(int i = 0; i < n; i++){
-            sum -= nums[i];
+        for(int i = 0; i < nums.length; i++){
+            result ^= (i + 1) ^ nums[i];
         }
         
-        return sum;
+        return result;
     }
 }
