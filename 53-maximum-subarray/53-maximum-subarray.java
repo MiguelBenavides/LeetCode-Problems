d734aeb51496cb119dabd4a13a84aab41df85ff3
Miguel Benavides Banda <36273644+MiguelBenavides@users.noreply.github.com>
2022-01-20 14:42:03 -0600

    Time: 2 ms (53.39%), Space: 90.4 MB (16.55%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) throw new IllegalArgumentException("Invalid parameter: null or empty");
+        
+        int result = nums[0];
+        int currSum = result;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] >= currSum && currSum <= 0){
+                currSum = nums[i];
+            } else {
+                currSum += nums[i];
+            }
+            result = Math.max(result, currSum);
+        }
+        
+        return result;
+    }
+}
