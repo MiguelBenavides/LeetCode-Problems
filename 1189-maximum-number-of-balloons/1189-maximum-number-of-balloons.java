a79696ef58c240fc5d7ea5df2df42f6b7d0eaa7e
Miguel Benavides Banda <36273644+MiguelBenavides@users.noreply.github.com>
2022-01-24 13:43:08 -0600

    Time: 13 ms (13.24%), Space: 39.9 MB (9.17%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -14,13 +14,11 @@ class Solution {
         //Put the word text in a HashMap and count each character (only for balloon letters)
         HashMap<Character, Integer> textMap = new HashMap<>();
         for(char c : text.toCharArray()){
-            if(balloonMap.containsKey(c)){
                 if(textMap.containsKey(c)){
                     textMap.put(c, textMap.get(c) + 1);
                 } else {
                     textMap.put(c, 1);
                 }
-            }
         }
         
         //Maximum amount of balloons cannot be bigger than initial text
