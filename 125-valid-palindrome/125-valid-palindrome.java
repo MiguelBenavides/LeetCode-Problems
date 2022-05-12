fab8bf84ab142ee4686473cf8d1b2a145e0cbdc1
Miguel Benavides Banda <36273644+MiguelBenavides@users.noreply.github.com>
2022-05-12 10:15:27 -0500

    Time: 11 ms (39.39%), Space: 44.3 MB (34.72%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder content = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) 
+            if(Character.isLetterOrDigit(s.charAt(i)))
+                content.append(s.charAt(i));
+        content = new StringBuilder(content.toString().replace(" ", "").toLowerCase());
+        String value = content.toString();
+        return value.equals(content.reverse().toString());
+    }
+}
